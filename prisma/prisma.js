da5c2a4ddcff35889a3020fe3948f372cf05d57d
DESKTOP-FR5AHN7\knyaz <knyazew.sv@gmail.com>
2023-09-10 23:37:27 +0300

    1

--- prisma/prisma.js
+++ prisma/prisma.js
@@ -1,6 +1,3 @@
 import { PrismaClient } from "@prisma/client";
 
-global.prisma = global.prisma || new PrismaClient();
-
-if (process.env.NODE_ENV !== "development") global.prisma = global.prisma;
-module.exports = global.prisma;
+export default prisma = new PrismaClient();
